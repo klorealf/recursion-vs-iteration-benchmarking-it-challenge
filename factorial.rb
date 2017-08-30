221eb4a7e179510a56910fa6114242c6cfd82409
bradsloan & Andreas Bach Landgrebe <pair+bsloan3.grebes15@devbootcamp.com>
2017-08-30 12:05:02 -0400

    run benchmark test on files

--- factorial.rb
+++ factorial.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 def factorial_iterative(n)
   (1..n).reduce(1, :*)
 end
@@ -5,4 +7,23 @@ end
 def factorial_recursive(n)
   return 1 if n < 2
   n * factorial_recursive(n - 1)
+end
+
+
+Benchmark.bm(500) do |x|
+  x.report("factorial_iterative for zero") {factorial_iterative(0)}
+  x.report("factorial_iterative for one thousand") {factorial_iterative(1000)}
+  x.report("factorial_iterative for two thousand") {factorial_iterative(2000)}
+  x.report("factorial_iterative for three thousand") {factorial_iterative(3000)}
+  x.report("factorial_iterative for four thousand") {factorial_iterative(4000)}
+  x.report("factorial_iterative for five thousand") {factorial_iterative(5000)}
+end
+
+Benchmark.bm(500) do |x|
+  x.report("factorial_recursive for zero") {factorial_recursive(0)}
+  x.report("factorial_recursive for one thousand") {factorial_recursive(1000)}
+  x.report("factorial_recursive for two thousand") {factorial_recursive(2000)}
+  x.report("factorial_recursive for three thousand") {factorial_recursive(3000)}
+  x.report("factorial_recursive for four thousand") {factorial_recursive(4000)}
+  x.report("factorial_recursive for five thousand") {factorial_recursive(5000)}
 end
--- fibonacci.rb
+++ fibonacci.rb
@@ -1,3 +1,5 @@
+require 'benchmark'
+
 def iterative_nth_fibonacci_number(n)
   sequence = [0, 1]
 
@@ -13,4 +15,22 @@ def recursive_nth_fibonacci_number(n)
   return 1 if n <= 2
 
   recursive_nth_fibonacci_number(n - 1) + recursive_nth_fibonacci_number(n - 2)
+end
+
+Benchmark.bm(500) do |x|
+  x.report("iterative_nth_fibonacci_number for zero") {iterative_nth_fibonacci_number(0)}
+  x.report("iterative_nth_fibonacci_number for one thousand") {iterative_nth_fibonacci_number(1000)}
+  x.report("iterative_nth_fibonacci_number for two thousand") {iterative_nth_fibonacci_number(2000)}
+  x.report("iterative_nth_fibonacci_number for three thousand") {iterative_nth_fibonacci_number(3000)}
+  x.report("iterative_nth_fibonacci_number for four thousand") {iterative_nth_fibonacci_number(4000)}
+  x.report("iterative_nth_fibonacci_number for five thousand") {iterative_nth_fibonacci_number(5000)}
+end
+
+Benchmark.bm(500) do |x|
+  x.report("recursive_nth_fibonacci_number for zero") {recursive_nth_fibonacci_number(0)}
+  x.report("recursive_nth_fibonacci_number for one thousand") {recursive_nth_fibonacci_number(1000)}
+  x.report("recursive_nth_fibonacci_number for two thousand") {recursive_nth_fibonacci_number(2000)}
+  x.report("recursive_nth_fibonacci_number for three thousand") {recursive_nth_fibonacci_number(3000)}
+  x.report("recursive_nth_fibonacci_number for four thousand") {recursive_nth_fibonacci_number(4000)}
+  x.report("recursive_nth_fibonacci_number for five thousand") {recursive_nth_fibonacci_number(5000)}
 end
