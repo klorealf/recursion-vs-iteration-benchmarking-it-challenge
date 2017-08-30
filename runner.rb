f5e658ef152b65c0424229a630d35d1f3fd2da64
Thomas Rafferty & Chris Camp <pair+ThomasRafferty.cpcamp2@devbootcamp.com>
2017-08-30 15:29:38 -0400

    Complete release 2 - build graphs to compair

--- runner.rb
+++ runner.rb
@@ -1,2 +1,52 @@
 require_relative 'factorial'
 require_relative 'fibonacci'
+
+require 'benchmark'
+
+# n = 500
+#   Benchmark.bm do |times|
+#   times.report {1.upto(n) do ; factorial_iterative(0); end}
+#   times.report {1.upto(n) do ; factorial_iterative(1000); end}
+#   times.report {1.upto(n) do ; factorial_iterative(2000); end}
+#   times.report {1.upto(n) do ; factorial_iterative(3000); end}
+#   times.report {1.upto(n) do ; factorial_iterative(4000); end}
+#   times.report {1.upto(n) do ; factorial_iterative(5000); end}
+#   times.report {1.upto(n) do ; factorial_iterative(10000); end}
+# end
+
+# n = 500
+#   Benchmark.bm do |times|
+#   times.report {1.upto(n) do ; factorial_recursive(0); end}
+#   times.report {1.upto(n) do ; factorial_recursive(1000); end}
+#   times.report {1.upto(n) do ; factorial_recursive(2000); end}
+#   times.report {1.upto(n) do ; factorial_recursive(3000); end}
+#   times.report {1.upto(n) do ; factorial_recursive(4000); end}
+#   times.report {1.upto(n) do ; factorial_recursive(5000); end}
+#   times.report {1.upto(n) do ; factorial_recursive(10000); end}
+# end
+
+n = 500
+  Benchmark.bm do |times|
+  times.report {1.upto(n) do ;iterative_nth_fibonacci_number(0); end}
+  times.report {1.upto(n) do ; iterative_nth_fibonacci_number(5); end}
+  times.report {1.upto(n) do ; iterative_nth_fibonacci_number(10); end}
+  times.report {1.upto(n) do ; iterative_nth_fibonacci_number(15); end}
+  times.report {1.upto(n) do ; iterative_nth_fibonacci_number(20); end}
+  times.report {1.upto(n) do ; iterative_nth_fibonacci_number(25); end}
+  times.report {1.upto(n) do ; iterative_nth_fibonacci_number(30); end}
+  # times.report {1.upto(n) do ; iterative_nth_fibonacci_number(35); end}
+end
+
+n = 500
+  Benchmark.bm do |times|
+  times.report {1.upto(n) do ; recursive_nth_fibonacci_number(0); end}
+  times.report {1.upto(n) do ; recursive_nth_fibonacci_number(5); end}
+  times.report {1.upto(n) do ; recursive_nth_fibonacci_number(10); end}
+  times.report {1.upto(n) do ; recursive_nth_fibonacci_number(15); end}
+  times.report {1.upto(n) do ; recursive_nth_fibonacci_number(20); end}
+  times.report {1.upto(n) do ; recursive_nth_fibonacci_number(25); end}
+  times.report {1.upto(n) do ; recursive_nth_fibonacci_number(30); end}
+  # times.report {1.upto(n) do ; recursive_nth_fibonacci_number(35); end}
+end
+
+
