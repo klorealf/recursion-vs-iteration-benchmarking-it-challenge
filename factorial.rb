9cdf2fc239922b5819785cddb7928238b4566622
Amber Maillard & klorealf <pair+amber2394.klorealf@devbootcamp.com>
2017-08-30 15:41:13 -0400

    add iterative and recursive benchmark module

--- factorial.rb
+++ factorial.rb
@@ -1,3 +1,5 @@
+
+
 def factorial_iterative(n)
   (1..n).reduce(1, :*)
 end
@@ -6,3 +8,19 @@ def factorial_recursive(n)
   return 1 if n < 2
   n * factorial_recursive(n - 1)
 end
+
+p factorial_iterative(0)
+
+p factorial_iterative(1000)
+
+
+# array = [0, 1000, 2000, 3000, 4000, 5000]
+
+# 500.times do
+#  array.each do |number|
+#     p result =  factorial_iterative(number)
+#   end
+# end
+
+
+
--- runner.rb
+++ runner.rb
@@ -1,2 +1,13 @@
 require_relative 'factorial'
 require_relative 'fibonacci'
+require 'benchmark'
+
+i=0
+while i <= 5000
+  puts "Iterative: #{i}"
+  puts Benchmark.measure {factorial_iterative(i)* 500 }
+
+  puts "Recursive: #{i}"
+  puts Benchmark.measure {factorial_iterative(i)* 500 }
+  i += 1000
+end
